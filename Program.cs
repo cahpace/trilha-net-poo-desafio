00561707f15aaea29e2046ec201483e425539448
Carolina Pace <carolina.pacesilva@gmail.com>
2024-01-09 11:49:27 -0300

    Pushing my changes on the exercise

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+//Testando Iphone
+Console.WriteLine("Teste Iphone: ");
+Iphone iphone1 = new Iphone(numero: "996538542", modelo: "15 Pro", imei: "85632242", memoria: 128);
+iphone1.Ligar();
+iphone1.InstalarAplicativo("WhatsApp");
+
+//Testando Nokia
+Console.WriteLine("Teste Nokia: ");
+Iphone nokia1 = new Iphone(numero: "998552364", modelo: "rgt", imei: "8965421", memoria: 64);
+nokia1.Ligar();
+nokia1.InstalarAplicativo("Google Maps");
+
